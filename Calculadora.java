bdb82e0be50eb73f4b68cd42cbe74b3d4b78b7fc
DeivodGMG <deivodyt@gmail.com>
2025-05-21 20:31:32 -0600

    Main: Codigo terminado.

--- Calculadora.java
+++ Calculadora.java
@@ -69,10 +69,10 @@ public class Calculadora{
    }
    //CLASE MAIN
    public static void main(String[] args){
-    //Instanciar objeto
+    //Instanciar objetos
     Calculadora calculadora = new Calculadora();
     Scanner scanner = new Scanner(System.in);
-
+    //Solicitar datos por consola
     System.out.println("Ingrese 3 numeros enteros:");
     int numInt1= scanner.nextInt();
     int numInt2= scanner.nextInt();
@@ -81,12 +81,35 @@ public class Calculadora{
     double numDouble1= scanner.nextDouble();
     double numDouble2= scanner.nextDouble();
     double numDouble3= scanner.nextDouble();
+    //Programa para sumar
     System.out.println("-------------------------");
     System.out.println("== SUMAR == ");
-    System.out.println("La suma de " + numInt1 + " y " + numInt2 + " = " + calculadora.Sumar(numInt1, numInt2) );
-    System.out.println("La suma de " + numInt1 + ", de " + numInt2 + " y de " + numInt3 + " = " + calculadora.Sumar(numInt1, numInt2, numInt3) );
-    System.out.println("La suma de " + numDouble1 + " y " + numDouble2 + " = " + calculadora.Sumar(numDouble1, numDouble2) );
-    System.out.println("La suma de " + numDouble1 + ", de " + numDouble2 + " y de " + numDouble3 + " = " + calculadora.Sumar(numDouble1, numDouble2, numDouble3) );
+    System.out.println("La suma de " + numInt1 + " + " + numInt2 + " = " + calculadora.Sumar(numInt1, numInt2) );
+    System.out.println("La suma de " + numInt1 + " + " + numInt2 + " + " + numInt3 + " = " + calculadora.Sumar(numInt1, numInt2, numInt3) );
+    System.out.println("La suma de " + numDouble1 + " + " + numDouble2 + " = " + calculadora.Sumar(numDouble1, numDouble2) );
+    System.out.println("La suma de " + numDouble1 + " + " + numDouble2 + " + " + numDouble3 + " = " + calculadora.Sumar(numDouble1, numDouble2, numDouble3) );
+    //Programa para restar
+    System.out.println("-------------------------");
+    System.out.println("== RESTAR == ");
+    System.out.println("La resta de " + numInt1 + " - " + numInt2 + " = " + calculadora.Resta(numInt1, numInt2) );
+    System.out.println("La resta de " + numInt1 + " - " + numInt2 + " - " + numInt3 + " = " + calculadora.Resta(numInt1, numInt2, numInt3) );
+    System.out.println("La resta de " + numDouble1 + " - " + numDouble2 + " = " + calculadora.Resta(numDouble1, numDouble2) );
+    System.out.println("La resta de " + numDouble1 + " - " + numDouble2 + " - " + numDouble3 + " = " + calculadora.Resta(numDouble1, numDouble2, numDouble3) );
+    //Programa para multiplicar
+    System.out.println("-------------------------");
+    System.out.println("== MULTIPLICACION == ");
+    System.out.println("La multiplicacion de " + numInt1 + " * " + numInt2 + " = " + calculadora.Multiplicacion(numInt1, numInt2) );
+    System.out.println("La multiplicacion de " + numInt1 + " * " + numInt2 + " * " + numInt3 + " = " + calculadora.Multiplicacion(numInt1, numInt2, numInt3) );
+    System.out.println("La multiplicacion de " + numDouble1 + " * " + numDouble2 + " = " + calculadora.Multiplicacion(numDouble1, numDouble2) );
+    System.out.println("La multiplicacion de " + numDouble1 + " * " + numDouble2 + " * " + numDouble3 + " = " + calculadora.Multiplicacion(numDouble1, numDouble2, numDouble3) );
+    //Programa para dividir
+    System.out.println("-------------------------");
+    System.out.println("== DIVISION == ");
+    System.out.println("La division de " + numInt1 + " / " + numInt2 + " = " + calculadora.Division(numInt1, numInt2) );
+    System.out.println("La division de " + numInt1 + " / " + numInt2 + " / " + numInt3 + " = " + calculadora.Division(numInt1, numInt2, numInt3) );
+    System.out.println("La division de " + numDouble1 + " / " + numDouble2 + " = " + calculadora.Division(numDouble1, numDouble2) );
+    System.out.println("La division de " + numDouble1 + " / " + numDouble2 + " / " + numDouble3 + " = " + calculadora.Division(numDouble1, numDouble2, numDouble3) );
+
     }
 
 }
